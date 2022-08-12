4f579b8ede64bb6bae3f6ab5007bee483d5b098e
EC2 Default User <makoto.se1988@outlook.jp>
2022-08-12 07:41:59 +0000

    [追加]ルーティング（管理者+会員）

--- config/routes.rb
+++ config/routes.rb
@@ -2,56 +2,51 @@ Rails.application.routes.draw do
   
   ## デフォルト... 8/10
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
-  ## 自動生成されたもの
-  # devise_for :customers   自動生成
-  # devise_for :admins      自動生成
-  
-  root to: 'public/homes#top'
-  
+ 
+  # devise_for :admins      自動生成... 8/10
   devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
   
-  namespace :admin do
-    # get '' => 'homes#top'
-    get 'top' => 'homes#top'
-    get 'orders/show'
-    get 'contact_genres/index'
-    get 'contact_genres/edit'
-    get 'contacts/index'
-    get 'contacts/show'
-    get 'costomers/index'
-    get 'costomers/show'
-    get 'costomers/edit'
-    get 'items_genres/index'
-    get 'items_genres/edit'
-    get 'items/index'
-    get 'items/new'
-    get 'items/show'
-    get 'items/edit'
+  namespace :admin do   # アドミンルーティング... 8/12
+    get '' => 'homes#top'
+    resources :orders,          only: [:show, :update] do
+      resources :order_details, only: [:update]
+    end
+    resources :contact_genres,  only: [:index, :create, :edit, :update]
+    resources :contacts,        only: [:index, :show]
+    resources :customers,       only: [:index, :show, :edit, :update]
+    resources :items_genres,    only: [:index, :create, :edit, :update]
+    resources :items,           except: [:destroy]
   end
   
+  # devise_for :customers   自動生成... 8/10
   devise_for :customers, skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
-  namespace :public do
-    get 'shipping_addresses/index'
-    get 'shipping_addresses/edit'
-    get 'contacts/new'
-    get 'contacts/complete'
-    get 'orders/new'
-    get 'orders/complete'
-    get 'orders/index'
-    get 'orders/show'
-    get 'cart_items/index'
-    get 'customer/show'
-    get 'customer/edit'
-    get 'customer/unsubscribe'
-    get 'items/index'
-    get 'items/show'
+
+  scope module: :public do   # パブリックルーティング... 8/12
+    root 'homes#top'
+    
+    # customers/editのようにするとdeviseのルーティングとかぶってしまうためinformationを付け加えている。
+    get 'customers/mypage' => 'customers#show', as: 'mypage'
+    get 'customers/information/edit' => 'customers#edit', as: 'edit_information'
+    patch 'customers/information' => 'customers#update', as: 'update_information'
+    get 'customers/unsubscribe' => 'customers#unsubscribe', as: 'confirm_unsubscribe'
+    put 'customers/information' => 'customers#update'
+    patch 'customers/withdraw' => 'customers#withdraw', as: 'withdraw_customer'
+    
+    resources :shipping_addresses,  only: [:index, :edit, :create, :update, :destroy]
+    
+    resources :items, only: [:index, :show]
+    
+    resources :cart_items,          only: [:index, :create, :update, :destroy]
+    delete 'cart_items/destroy_all' => 'cart_items#destroy_all', as: 'destroy_all_cart_items'
+    
+    resources :orders, only: [:new, :index, :create, :show]
+    post 'orders/confirm' => 'orders#confirm'
+    get 'orders/complete' => 'orders#complete', as: 'complete'
   end
   
 end
