b506664f1f73ec1d84da38bdf2c962c6d80f3674
EC2 Default User <makoto.se1988@outlook.jp>
2022-08-21 18:06:21 +0000

    [追加]管理者-注文ステータス更新機能

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,20 @@
 class Admin::OrdersController < ApplicationController
+  
   def show
+    @order = Order.find(params[:id])
+    @orders = Order.all
   end
+  
+  def update
+    order = Order.find(params[:id])
+    order.update(order_params)
+    redirect_to admin_order_path(order.id), notice: "ステータスが変更されました。"
+  end
+  
+  private
+  
+  def order_params
+    params.require(:order).permit(:customer_id, :postal_code, :address, :name, :postage, :payment_of_total, :payment_method, :order_status)
+  end
+  
 end
