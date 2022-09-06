a810e5b8aa0fa92a0d08aa41034fc6e8fdd720c4
EC2 Default User <makoto.se1988@outlook.jp>
2022-09-06 02:43:34 +0000

    [９割完成(レ)]会員-商品一覧,詳細

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,8 @@ class Item < ApplicationRecord
     has_many :cart_items, dependent: :destroy
     has_many :order_histories, dependent: :destroy
     
+    
+    def add_tax_price
+        (self.price * 1.10).round
+    end
 end
