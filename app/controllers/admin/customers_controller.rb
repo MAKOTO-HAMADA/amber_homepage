79ecd53f268dfa07d2be36dae6edbc13527d44e5
EC2 Default User <makoto.se1988@outlook.jp>
2022-09-19 14:56:44 +0000

    [完成(機)]管理者>エラー>注文履歴タブ+顧客一覧タブ

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -13,15 +13,20 @@ class Admin::CustomersController < ApplicationController
   end
   
   def update
-    customer = Customer.find(params[:id])
-    customer.update(customer_params)
-    redirect_to admin_customer_path
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      bypass_sign_in(@customer)
+      flash[:notice] = "編集を完了しました。"
+      redirect_to admin_customer_path
+    else
+      render :edit
+    end
   end
   
   private
   
   def customer_params
-    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :telephone_number, :postal_code, :prefecture_code, :city, :street, :other_address, :password, :password_confirmation, :unsubscribe_flag)
+    params.require(:customer).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :email, :telephone_number, :postal_code, :prefecture_code, :city, :street, :other_address, :unsubscribe_flag)
   end
   
 end
